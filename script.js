02f6cd87b7306c7adc98f205f12847af0d7edcca
Suprio Roy <suprio.roy94@gmail.com>
2021-08-14 23:16:59 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+let change_background_color= setInterval(changeColor,10000);
+let displayTimer= setInterval(showtimer,1000);
+
 function changeColor(){
     var hex_numbers= ["0","1","2","3","4","5","6","7","8",
 "9","A","B","C","D","E","F"]
@@ -13,3 +16,11 @@ document.getElementById ("hex-code").innerHTML=hexcode;
 document.getElementsByTagName("body")[0].style.backgroundColor= "#" + hexcode;
 
 }
+
+function showtimer(){
+let currentDateTime= new Date();
+let currentTime= currentDateTime.toLocaleTimeString();
+document.getElementById("timer").innerHTML=currentTime
+}
+
+
